b85b6ab89c3c4c450a78de12d42c7bc37bda84d8
Bojan <bojan.bosnic97@gmail.com>
2020-11-04 20:31:21 +0100

    interpolate

--- webpack.common.js
+++ webpack.common.js
@@ -21,7 +21,7 @@ module.exports = {
       },
     module: {
         rules: [
-            {
+            { 
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: [
